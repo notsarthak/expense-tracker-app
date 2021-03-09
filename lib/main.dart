ff201564a14ecb0837bc73c15de18a59bd27bf77
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-03-10 00:27:55 +0530

    Styling the transaction details (Amount container) in the card

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,17 @@ class MyHomePage extends StatelessWidget {
                 return Card(
                   child: Row(children: <Widget>[
                     Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
                       child: Text(
                         tx.amount.toString(),
                       ),
