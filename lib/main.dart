742caaf5ac86646b47d011517be3f4f763c5da7c
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-03-10 00:59:37 +0530

    Styling the transaction details (Title and date) in the card

--- lib/main.dart
+++ lib/main.dart
@@ -62,22 +62,40 @@ class MyHomePage extends StatelessWidget {
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         ),
                       ),
                       child: Text(
                         tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
                       ),
                     ),
-                    Column(children: <Widget>[
-                      Text(
-                        tx.title,
-                      ),
-                      Text(
-                        tx.date.toString(),
-                      ),
-                    ]),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(bottom: 4),
+                            child: Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 14,
+                            ),
+                          ),
+                        ]),
                   ]),
                 );
               }).toList()
