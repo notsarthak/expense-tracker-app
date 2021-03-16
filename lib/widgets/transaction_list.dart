29fc5ebb41ed7c1f9ebec2e0e4e007987a58b432
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-03-16 17:07:36 +0530

    Image widget, SizedBox widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,54 +12,72 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
+      child: transactions.length == 0
+          ? Column(
+              children: <Widget>[
+                Text(
+                  "No expenses yet!",
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2,
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-                child: Text(
-                  '\$' + transactions[index].amount.toStringAsFixed(2),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Theme.of(context).primaryColor,
-                  ),
+                Container(
+                  height: 200,
+                  child: Image.asset('assets/images/waiting.png',
+                      fit: BoxFit.cover),
                 ),
-              ),
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(children: <Widget>[
                     Container(
-                      margin: EdgeInsets.only(bottom: 4),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2,
+                        ),
+                      ),
                       child: Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6,
+                        '\$' + transactions[index].amount.toStringAsFixed(2),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor,
+                        ),
                       ),
                     ),
-                    Text(
-                      DateFormat.yMMMMEEEEd().format(transactions[index].date),
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 14,
-                      ),
-                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(bottom: 4),
+                            child: Text(
+                              transactions[index].title,
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMMEEEEd()
+                                .format(transactions[index].date),
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 14,
+                            ),
+                          ),
+                        ]),
                   ]),
-            ]),
-          );
-        },
-      ),
+                );
+              },
+            ),
     );
   }
 }
