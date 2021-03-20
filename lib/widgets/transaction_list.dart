2a0d103ccfac506d851dd9d74dee6c549d996a04
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-03-20 20:40:26 +0530

    ListTile widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,6 +3,85 @@ import "package:intl/intl.dart";
 
 import "../models/transaction.dart";
 
+// class TransactionList extends StatelessWidget {
+//   final List<Transaction> transactions;
+
+//   TransactionList(this.transactions);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 300,
+//       child: transactions.length == 0
+//           ? Column(
+//               children: <Widget>[
+//                 Text(
+//                   "No expenses yet!",
+//                   style: Theme.of(context).textTheme.headline6,
+//                 ),
+//                 SizedBox(
+//                   height: 20,
+//                 ),
+//                 Container(
+//                   height: 200,
+//                   child: Image.asset('assets/images/waiting.png',
+//                       fit: BoxFit.cover),
+//                 ),
+//               ],
+//             )
+//           : ListView.builder(
+//               itemCount: transactions.length,
+//               itemBuilder: (ctx, index) {
+//                 return Card(
+//                   child: Row(children: <Widget>[
+//                     Container(
+//                       margin: EdgeInsets.symmetric(
+//                         vertical: 10,
+//                         horizontal: 15,
+//                       ),
+//                       padding: EdgeInsets.all(10),
+//                       decoration: BoxDecoration(
+//                         border: Border.all(
+//                           color: Theme.of(context).primaryColor,
+//                           width: 2,
+//                         ),
+//                       ),
+//                       child: Text(
+//                         '\$' + transactions[index].amount.toStringAsFixed(2),
+//                         style: TextStyle(
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 20,
+//                           color: Theme.of(context).primaryColor,
+//                         ),
+//                       ),
+//                     ),
+//                     Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: <Widget>[
+//                           Container(
+//                             margin: EdgeInsets.only(bottom: 4),
+//                             child: Text(
+//                               transactions[index].title,
+//                               style: Theme.of(context).textTheme.headline6,
+//                             ),
+//                           ),
+//                           Text(
+//                             DateFormat.yMMMMEEEEd()
+//                                 .format(transactions[index].date),
+//                             style: TextStyle(
+//                               color: Colors.grey[600],
+//                               fontSize: 14,
+//                             ),
+//                           ),
+//                         ]),
+//                   ]),
+//                 );
+//               },
+//             ),
+//     );
+//   }
+// }
+
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
@@ -33,48 +112,25 @@ class TransactionList extends StatelessWidget {
               itemCount: transactions.length,
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        ),
-                      ),
-                      child: Text(
-                        '\$' + transactions[index].amount.toStringAsFixed(2),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor,
+                  elevation: 6,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\$${transactions[index].amount}'),
                         ),
                       ),
                     ),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(bottom: 4),
-                            child: Text(
-                              transactions[index].title,
-                              style: Theme.of(context).textTheme.headline6,
-                            ),
-                          ),
-                          Text(
-                            DateFormat.yMMMMEEEEd()
-                                .format(transactions[index].date),
-                            style: TextStyle(
-                              color: Colors.grey[600],
-                              fontSize: 14,
-                            ),
-                          ),
-                        ]),
-                  ]),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMMd().format(transactions[index].date),
+                    ),
+                  ),
                 );
               },
             ),
