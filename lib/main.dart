c15577e3300b433201040f701fe3e77602be4e26
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-07-11 13:46:08 +0530

    App life cycle states

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class MyHomePage extends StatefulWidget {
   }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [
     // Transaction(
     //   id: "t1",
@@ -101,6 +101,23 @@ class _MyHomePageState extends State<MyHomePage> {
         return NewTransaction(_addNewTransaction);
       },
     );
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
   }
 
   List<Transaction> get _recentTransactions {
